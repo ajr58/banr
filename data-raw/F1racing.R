0073f6f03da3a08396580d309e54ffc92e206611
Brea Koenes <bjk47@students.calvin.edu>
2022-03-14 15:01:29 -0400

    R file for dataset

---
+++ data-raw/F1racing.R
@@ -0,0 +1,28 @@
+# library
+library(usethis)
+library(tidyverse)
+library(dplyr)
+require(plotly)
+
+# import data (from local .csv file)
+races <- read_csv("data/races.csv")
+status <- read_csv("data/status.csv")
+results <- read_csv("data/results.csv", col_types = cols(number = col_character()))
+
+# join datasets
+joined_results <- left_join(status, results, by='statusId') %>%
+  left_join(races, results, by='raceId')
+
+# wrangle data
+status_data <- joined_results %>%
+  mutate(status = if_else((status == "Accident") |
+                            (status == "Mechanical"),
+                          "Accident/Mechanical",
+                          status)) %>%                  # Mutate new a new category
+  filter(status == "Accident/Mechanical" |
+           status == "Finished") %>%                    # Filter 2 categories
+  group_by(status, year) %>%
+  summarise(status_sum = n())
+
+# add to package
+usethis::use_data(joined_results)
