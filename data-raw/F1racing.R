485c9dd2fd2a6044b458495ec76ed392b7ca550e
Brea Koenes <bjk47@students.calvin.edu>
2022-03-14 15:07:27 -0400

    changes to joined dataset's name

--- data-raw/F1racing.R
+++ data-raw/F1racing.R
@@ -10,11 +10,11 @@ status <- read_csv("data/status.csv")
 results <- read_csv("data/results.csv", col_types = cols(number = col_character()))
 
 # join datasets
-joined_results <- left_join(status, results, by='statusId') %>%
+F1racing <- left_join(status, results, by='statusId') %>%
   left_join(races, results, by='raceId')
 
 # wrangle data
-status_data <- joined_results %>%
+status_data <- F1racing %>%
   mutate(status = if_else((status == "Accident") |
                             (status == "Mechanical"),
                           "Accident/Mechanical",
@@ -25,4 +25,4 @@ status_data <- joined_results %>%
   summarise(status_sum = n())
 
 # add to package
-usethis::use_data(joined_results)
+usethis::use_data(F1racing)
