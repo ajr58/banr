d113c9c00dab4cf3e6ac73bc6436633403bef8be
ajr58 <ajr58@students.calvin.edu>
2022-03-16 19:21:55 -0400

    Add defaults to df_apply function

--- R/df_apply.R
+++ R/df_apply.R
@@ -1,6 +1,6 @@
 #' @importFrom tibble as_tibble
 
-df_apply <- function(.data, .f, .condition, .else, ...) {
+df_apply <- function(.data, .f, .condition = is.numeric, .else = identity, ...) {
     .data |>
     lapply(function(x) if (.condition(x)) .f(x, ...) else .else(x)) |>
     tibble::as_tibble()
