f0c750a283a0f781067ff7736898ba4fb0a64131
ajr58 <ajr58@students.calvin.edu>
2022-03-17 16:23:46 -0400

    Add documentation for df_apply and other changes

--- R/df_apply.R
+++ R/df_apply.R
@@ -1,7 +1,19 @@
+#' Apply a function to columns of a data frame satisfying a condition
+#'
+#' @param .data a data frame.
+#' @param .f a function to apply to columns of the data frame.
+#' @param .condition a function returning boolean values.
+#' @param .else a function to apply when .condition evaluates to false.
+#' @param ... additional arguments to be passed to .f.
+#'
+#' @return A tibble with updated columns.
+#'
+#' @examples
+#'
 #' @importFrom tibble as_tibble
 
 df_apply <- function(.data, .f, .condition = is.numeric, .else = identity, ...) {
     .data |>
     lapply(function(x) if (.condition(x)) .f(x, ...) else .else(x)) |>
-    tibble::as_tibble()
+    as_tibble()
 }
