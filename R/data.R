d377f24f98da4e427a1f9d2255af17750ad04919
ajr58 <ajr58@students.calvin.edu>
2022-03-18 17:24:16 -0400

    Add Documentation for Adam's data

--- R/data.R
+++ R/data.R
@@ -11,32 +11,48 @@
 #' @source \url{http://ergast.com/mrd/db/#csv/}
 "F1racing"
 
-#' Prices of 50,000 round cut diamonds.
+#' Food Data of Animal and non-Animal Products
 #'
-#' A dataset containing the prices and other attributes of almost 54,000
-#' diamonds.
+#' A dataset containing the global consumption and carbon emissions of Animal
+#' and non-Animal products.
 #'
-#' @format A data frame with 53940 rows and 10 variables:
+#' @format A data frame with 2 rows and 6 variables:
 #' \describe{
-#'   \item{price}{price, in US dollars}
-#'   \item{carat}{weight of the diamond, in carats}
+#'   \item{food_type}{Animal Product or non-Animal Product}
+#'   \item{total_consumption}{global consumption, in kg/person/year}
+#'   \item{total_emission}{global CO2 emission, in kg/person/year}
+#'   \item{perc_consumption}{percentage of total global food consumption}
+#'   \item{perc_emission}{percentage of total global food CO2 emission}
+#'   \item{% of Total}{placeholder for graph label}
 #'   ...
 #' }
-#' @source \url{http://www.diamondse.info/}
+#' @source \url{https://www.nu3.de/blogs/nutrition/food-carbon-footprint-index-2018}
+#'
+#' @example
+#'
+#' library(ggplot2)
+#' library(patchwork)
+#' plot1 <- ggplot(food_consumption_grouped, aes(fill=food_type, y=perc_consumption, x=`% of Total`)) +
+#'     geom_bar(position="fill", stat="identity") + coord_flip()
+#' plot2 <- ggplot(food_consumption_grouped, aes(fill=food_type, y=perc_emission, x=`% of Total`)) +
+#'     geom_bar(position="fill", stat="identity") + coord_flip()
+#' plot1 / plot2
 "food_consumption_grouped"
 
-#' Prices of 50,000 round cut diamonds.
+#' Food Data of All Food Groups
 #'
-#' A dataset containing the prices and other attributes of almost 54,000
-#' diamonds.
+#' A dataset containing the global consumption and carbon emissions of
+#' different Food Groups
 #'
-#' @format A data frame with 53940 rows and 10 variables:
+#' @format A data frame with 11 rows and 4 variables:
 #' \describe{
-#'   \item{price}{price, in US dollars}
-#'   \item{carat}{weight of the diamond, in carats}
+#'   \item{food_category}{food group}
+#'   \item{food_type}{Animal Product or non-Animal Product}
+#'   \item{total_consumption}{global consumption, in kg/person/year}
+#'   \item{total_emission}{global CO2 emission, in kg/person/year}
 #'   ...
 #' }
-#' @source \url{http://www.diamondse.info/}
+#' @source \url{https://www.nu3.de/blogs/nutrition/food-carbon-footprint-index-2018}
 "food_consumption_total"
 
 #' Prices of 50,000 round cut diamonds.
