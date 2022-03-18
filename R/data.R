4b4d58352e907999e5a89ee5b45b3dc9eb130c0f
Brea Koenes <bjk47@students.calvin.edu>
2022-03-18 16:01:26 -0400

    added documentation

--- R/data.R
+++ R/data.R
@@ -1,15 +1,14 @@
-#' Prices of 50,000 round cut diamonds.
+#' Statuses of Formula 1 races
 #'
-#' A dataset containing the prices and other attributes of almost 54,000
-#' diamonds.
+#' A dataset containing attributes of Formula 1 races over the years.
 #'
-#' @format A data frame with 53940 rows and 10 variables:
+#' @format A data frame with 139 rows and 3 variables:
 #' \describe{
-#'   \item{price}{price, in US dollars}
-#'   \item{carat}{weight of the diamond, in carats}
+#'   \item{status}{finished or accident/mechanical failure}
+#'   \item{status_sum}{count of the statuses for a given year}
 #'   ...
 #' }
-#' @source \url{http://www.diamondse.info/}
+#' @source \url{http://ergast.com/mrd/db/#csv/}
 "F1racing"
 
 #' Prices of 50,000 round cut diamonds.
